15c504dc25772e6a5bd6899c3021c60ca025bfea
ISAAC KLETZLI <u1243853@umail.utah.edu>
2023-09-15 10:32:32 -0600

    Commented and cleaned up code

--- DecisionTree/hw1.py
+++ DecisionTree/hw1.py
@@ -1,6 +1,12 @@
 import os
 import math
 
+class Node:
+    def __init__(self, children, attribute, label):
+        self.children = children
+        self.attribute = attribute
+        self.label = label
+
 class Purity:
     def __init__(self):
         pass
@@ -36,16 +42,6 @@ class GiniIndex(Purity):
         sum_prob_square = sum([p * p for p in probabilities])
         return 1.0 - sum_prob_square
 
-class Node:
-    def __init__(self, children, attribute, label):
-        self.children = children
-        self.attribute = attribute
-        self.label = label
-
-    # Instance method
-    def method1(self):
-        print("")
-
 def get_label_counts(examples):
     label_counts = {}
 
@@ -59,6 +55,7 @@ def get_label_counts(examples):
     
     return label_counts
 
+# ID3 algorithm to return a decision tree
 def ID3(examples, attributes_and_vals, purity_measure, max_depth):
     attributes = list(attributes_and_vals.keys())
     dif_labels = False
@@ -68,14 +65,17 @@ def ID3(examples, attributes_and_vals, purity_measure, max_depth):
 
     label_counts = get_label_counts(examples)
 
+    # check if labels are all the same
     for example in examples:
         label = example["label"]
         if (label != first_label):
             dif_labels = True
 
+    # Base case 1: labels are all the same
     if dif_labels == False:
         return Node(None, None, first_label)
     
+    # Base case 2: no more attributes, or depth is exceeded
     if len(attributes) == 0 or max_depth == 0:
         best_count = 0
         best_label = ""
@@ -87,6 +87,7 @@ def ID3(examples, attributes_and_vals, purity_measure, max_depth):
 
         return Node(None, None, best_label)
 
+    # create root node
     root_node = Node(None, None, None)
 
     total_purity = purity_measure.purity(label_counts, len(examples))
@@ -94,6 +95,7 @@ def ID3(examples, attributes_and_vals, purity_measure, max_depth):
     best_gain = 0
     best_attribute = attributes[0]
 
+    # find best attribute using the purity measure
     for attribute in attributes:
         attribute_vals = {}
         for example in examples:
@@ -123,13 +125,16 @@ def ID3(examples, attributes_and_vals, purity_measure, max_depth):
     for attribute_val in attributes_and_vals[best_attribute]:
         children[attribute_val] = []
     
+    # split examples on the best_attribute found earlier
     for example in examples:
         attribute_val = example[best_attribute]
         children[attribute_val].append(example)
 
     children_of_root = {}
 
+
     for attribute_val, new_examples in children.items():
+        # set is empty, create a label using the majority label
         if (len(new_examples) == 0):
             best_count = 0
             best_label = ""
@@ -139,6 +144,7 @@ def ID3(examples, attributes_and_vals, purity_measure, max_depth):
                     best_count = count
                     best_label = label
             children_of_root[attribute_val] = Node(None, None, best_label)
+        # recurse on the new set
         else:
             new_attributes_and_vals = {}
             for attr, vals in attributes_and_vals.items():
@@ -149,6 +155,7 @@ def ID3(examples, attributes_and_vals, purity_measure, max_depth):
     root_node.children = children_of_root
     return root_node
 
+# converts the examples into a list of maps that map an example's attributes to its values
 def read_examples(file_name, attributes):
     script_directory = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(script_directory, file_name)
@@ -165,7 +172,8 @@ def read_examples(file_name, attributes):
     
     return examples
 
-def read_description(file_name):
+# reads the attributes for the car data
+def read_car_description(file_name):
     script_directory = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(script_directory, file_name)
 
@@ -186,6 +194,7 @@ def read_description(file_name):
     
     return attributes
 
+# reads the attributes for the bank data
 def read_bank_description(file_name):
     script_directory = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(script_directory, file_name)
@@ -214,11 +223,7 @@ def read_bank_description(file_name):
     
     return attributes
 
-def train_tree(train_data, purity_measure, max_depth):
-    attributes = read_description("car/data-desc.txt")
-    tree = ID3(train_data, attributes, purity_measure, max_depth)
-    return tree
-
+# get the prediction from the decision tree for the example
 def predict(tree, example):
     current_subtree = tree
     while current_subtree.label == None:
@@ -227,6 +232,7 @@ def predict(tree, example):
 
     return current_subtree.label
 
+# determines percent of correctly predicted labels
 def percent_predicted_correct(tree, examples):
     num_examples = len(examples)
     num_right = 0
@@ -242,18 +248,25 @@ def evaluate_car_tree(purity_measures):
     train_data = read_examples("car/train.csv", attributes)
     test_data = read_examples("car/test.csv", attributes)
 
-    attributes = read_description("car/data-desc.txt")
+    print("Evalutating car data:\n")
+
+    attributes = read_car_description("car/data-desc.txt")
     for purity_measure in purity_measures:
+        class_name = purity_measure.__class__.__name__
+        print(f"Purity: {class_name}")
+
         for depth in range(6):
             tree = ID3(train_data, attributes, purity_measure, depth + 1)
             test_err = percent_predicted_correct(tree, test_data)
             train_err = percent_predicted_correct(tree, train_data)
 
             class_name = purity_measure.__class__.__name__
-            print(f"Purity: {class_name}, Depth: {depth + 1}, Test Error: {test_err}, Train Error: {train_err}")
+            print(f"Depth: {depth + 1}, Test Error: {test_err}, Train Error: {train_err}")
         
         print("")
 
+# converts numeric data to categorical by comparing numeric values to median and 
+# determining if the value is bigger or smaller than the median
 def numeric_to_categorical(train_data, test_data, attributes):
     for name, vals in attributes.items():
         if vals[0] == 'numeric':
@@ -281,6 +294,7 @@ def numeric_to_categorical(train_data, test_data, attributes):
             
             attributes[name] = ['bigger', 'smaller']
 
+# updates unknown values with the majority label for that attribute
 def update_unknown_values(train_data, test_data, attributes):
     majority_values = {}
 
@@ -330,27 +344,34 @@ def evaluate_bank_tree(purity_measures):
 
     attributes.pop('label')
 
+    print("Evalutating bank data with 'unknown' as an attribute value:\n")
+
     for purity_measure in purity_measures:
+        class_name = purity_measure.__class__.__name__
+        print(f"Purity: {class_name}")
         for depth in range(16):
             tree = ID3(train_data, attributes, purity_measure, depth + 1)
             test_err = percent_predicted_correct(tree, test_data)
             train_err = percent_predicted_correct(tree, train_data)
 
-            class_name = purity_measure.__class__.__name__
-            print(f"Purity: {class_name}, Depth: {depth + 1}, Test Error: {test_err}, Train Error: {train_err}")
+            print(f"Depth: {depth + 1}, Test Error: {test_err}, Train Error: {train_err}")
         
         print("")
 
     update_unknown_values(train_data, test_data, attributes)
 
+    print("Evalutating bank data with 'unknown' as a missing attribute value:\n")
+
     for purity_measure in purity_measures:
+        class_name = purity_measure.__class__.__name__
+        print(f"Purity: {class_name}")
         for depth in range(16):
             tree = ID3(train_data, attributes, purity_measure, depth + 1)
             test_err = percent_predicted_correct(tree, test_data)
             train_err = percent_predicted_correct(tree, train_data)
 
             class_name = purity_measure.__class__.__name__
-            print(f"Purity: {class_name}, Depth: {depth + 1}, Test Error: {test_err}, Train Error: {train_err}")
+            print(f"Depth: {depth + 1}, Test Error: {test_err}, Train Error: {train_err}")
         
         print("")
 
@@ -360,8 +381,8 @@ def main():
     purity_measures.append(MajorityError())
     purity_measures.append(InformationGain())
 
-    evaluate_bank_tree(purity_measures)
     evaluate_car_tree(purity_measures)
+    evaluate_bank_tree(purity_measures)
 
 if __name__ == "__main__":
     main()
